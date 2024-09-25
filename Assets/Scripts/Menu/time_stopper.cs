f33b2e23411ceaca44e5f94a0ed03cffb005efc3
twilinger <rafaelasdeveloper@gmail.com>
2024-09-25 07:38:24 +0300

    UX update

---
+++ Assets/Scripts/Menu/time_stopper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time_stopper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+    public void ContinueTime()
+    {
+
+    Time.timeScale = 1f;
+    }
+    
+}
